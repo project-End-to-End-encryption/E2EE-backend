2e67e66501920ca8dc276ba72b3d6da08f26762c
Vivek Anand Shaw <myself.vivekanandshaw@gmail.com>
2026-09-01 11:36:42 +0530

    BREAKING CHANGE: server setup

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express();
+
+app.use(express.json());
+
+
+app.get('/test', (req,res)=>{
+    res.json({
+        message: "Server is running"
+    });
+})
+
+module.exports = app;
