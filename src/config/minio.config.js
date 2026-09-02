e2280d77bf52d045115b0b1f991b3ddf9c87c6f8
Vivek Anand Shaw <myself.vivekanandshaw@gmail.com>
2026-09-02 21:28:50 +0530

    minio config add

---
+++ src/config/minio.config.js
@@ -0,0 +1,20 @@
+const minioConfig = {
+    endpoint: process.env.MINIO_ENDPOINT,
+
+    port: process.env.MINIO_PORT,
+
+    useSSL: process.env.MINIO_USE_SSL,
+
+    credentials: {
+        accessKey: process.env.MINIO_ACCESS_KEY,
+        secretKey: process.env.MINIO_SECRET_KEY,
+    },
+
+    bucket: {
+        profileAssets: process.env.MINIO_PROFILE_BUCKET,
+
+        encryptedAttachment: process.env.MINIO_ENCRYPTED_BUCKET
+    }
+};
+
+export default minioConfig;
