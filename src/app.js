43a371a1315205e1eda2e22af1bf52080987b79b
Vivek Anand Shaw <myself.vivekanandshaw@gmail.com>
2026-09-10 13:32:28 +0530

    cors fixed 4

--- src/app.js
+++ src/app.js
@@ -5,46 +5,19 @@ import routes from "./routes/index.js";
 import cookieParser from 'cookie-parser'
 import {BadRequestException} from "./shared/errors/domainErrors.js";
 import cors from 'cors';
-
-const corsOptions = {
-    origin: "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-};
-
-app.use((req, res, next) => {
-    console.log(">>> REQUEST:", req.method, req.originalUrl);
-    next();
-});
-
-app.use(cors(corsOptions));
-
-app.options(/.*/, (req, res) => {
-    console.log(">>> OPTIONS HANDLER HIT");
-
-    res.header(
-        "Access-Control-Allow-Origin",
-        "http://localhost:5173"
-    );
-    res.header(
-        "Access-Control-Allow-Credentials",
-        "true"
-    );
-    res.header(
-        "Access-Control-Allow-Methods",
-        "GET,POST,PUT,PATCH,DELETE,OPTIONS"
-    );
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Content-Type, Authorization"
-    );
-
-    res.sendStatus(204);
-});
-
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 
+const corsOptions = {
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.options("*path", cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
