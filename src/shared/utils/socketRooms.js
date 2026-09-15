684f98e179274e4b19e74610ce77d06528c81fd2
Vivek Anand Shaw <myself.vivekanandshaw@gmail.com>
2026-09-15 16:58:40 +0530

    socketRoom improved

--- src/shared/utils/socketRooms.js
+++ src/shared/utils/socketRooms.js
@@ -1,3 +1,10 @@
 export const userRoom = (userId) => `user:${userId}`;
-export const deviceRoom = (userId, deviceId) => `device:${userId}:${deviceId}`;
-export const groupRoom = (groupId) => `group${groupId}`;
+export const deviceRoom = (userId, deviceId) => {
+    if(!userId || !deviceId){
+        throw new Error('deviceRoom requires both userId and deviceId');
+    }
+    return `device:${userId}:${deviceId}`;
+}
+export const conversationRoom = (conversationId) => `conv:${conversationId}`;
+
+export const groupRoom = conversationRoom;
